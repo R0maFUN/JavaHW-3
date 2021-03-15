efd087ed0e0255a3131311957525d245e3c4d221
Roma Balayan <roman.balayan@openmedia.co>
2021-03-15 11:21:28 +0300

    Fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-
         System.out.println("Enter the data. (E.g. Иванов Иван Иванович 17.12.1980)");
         Scanner cin = new Scanner(System.in);
         String input = cin.nextLine();
